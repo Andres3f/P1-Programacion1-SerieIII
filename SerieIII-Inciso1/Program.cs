968e21153cb29524a2e2867817632a58aee7fe2b
Andres3f <andres022jks@gmail.com>
2024-03-09 12:40:29 -0600

    parcial1

---
+++ SerieIII-Inciso1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerieIII_Inciso1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hola :), ingrese el precio del primer producto ");
+            string numeroIngresado = Console.ReadLine();
+            float num1 = float.Parse(numeroIngresado);
+            Console.WriteLine("Ingrese el precio del segundo producto: ");
+            string segundoNumeroIngresado = Console.ReadLine();
+            float num2 = float.Parse(segundoNumeroIngresado);
+            Console.WriteLine("Ingrese el precio del tercer producto: ");
+            string tercerNumeroIngresado = Console.ReadLine();
+            float num3 = float.Parse(segundoNumeroIngresado);
+            float resultado = num1 + num2 + num3;
+            Console.WriteLine("La suma de los precios {0} + {1} + {2} es de: {3}", num1, num2, num3, resultado);
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ SerieIII-Inciso2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneradorDeCorreoElectronico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Bienvenido!");
+            Console.Write("Por favor, ingresa tu nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ahora, ingresa tu apellido: ");
+            string apellido = Console.ReadLine();
+
+            Console.Write("Ingresa el dominio de correo electrónico (por ejemplo, gmail.com): ");
+            string dominio = Console.ReadLine();
+
+            string primerasLetrasNombre = nombre.Substring(0, Math.Min(nombre.Length, 3));
+            string primerasLetrasApellido = apellido.Substring(0, Math.Min(apellido.Length, 3));
+
+            string correoElectronico = $"{primerasLetrasNombre}{primerasLetrasApellido}@{dominio}";
+
+            Console.WriteLine($"Tu correo electrónico generado es: {correoElectronico}");
+        }
+    }
+}
